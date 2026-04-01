5a11aa62f5d935388917b6b87777512fc4c01d3b
Stef Jones <81389190+StefaniaLuisa@users.noreply.github.com>
2026-04-01 18:13:03 -0500

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  // This tells Vite to look in the /stupski-control-report/ folder for files
-  base: '/stupski-control-report/', 
-})
