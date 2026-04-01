4eada28f5fa63ee6522ac14d8f4bd2cad2979d11
Stef Jones <81389190+StefaniaLuisa@users.noreply.github.com>
2026-04-01 18:05:32 -0500

    Configure Vite base path for project
    Set the base path for Vite to '/stupski-control-report/'.

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  // This tells Vite to look in the /stupski-control-report/ folder for files
   base: '/stupski-control-report/', 
 })
